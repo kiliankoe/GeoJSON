6ab8fd46879fce2295bc1f7fc31b5b1ee64145ef
Kilian Koeltzsch <me@kilian.io>
2020-07-08 21:48:20 +0200

    Add missing type and init to Feature

--- Sources/GeoJSON/Feature.swift
+++ Sources/GeoJSON/Feature.swift
@@ -1,6 +1,12 @@
 import AnyCodable
 
 public struct Feature: Equatable, Codable {
+    let type = "Feature"
     public let geometry: Geometry
     public let properties: [String: AnyCodable]
+
+    public init(geometry: Geometry, properties: [String: AnyCodable] = [:]) {
+        self.geometry = geometry
+        self.properties = properties
+    }
 }
