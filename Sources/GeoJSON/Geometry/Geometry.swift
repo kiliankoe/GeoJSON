028c2d7a5d79481768b07e3781692ec61898d0c1
Kilian Koeltzsch <me@kilian.io>
2020-07-11 00:59:01 +0200

    add doc comment about avoiding nested geo collections

--- Sources/GeoJSON/Geometry/Geometry.swift
+++ Sources/GeoJSON/Geometry/Geometry.swift
@@ -13,7 +13,7 @@ public enum Geometry: Equatable, Codable {
     /// A collection of `Polygon`s.
     case multiPolygon(MultiPolygon)
     /// A heterogeneous composition of other `Geometry` objects. For example, a `Geometry` object in the shape of a lowercase roman "i" can be composed
-    /// of one `Point` and one `LineString`.
+    /// of one `Point` and one `LineString`. Please try and avoid nested `GeometryCollection`s to maximize interopability.
     /// See [RFC7946 Section 3.1.8](https://tools.ietf.org/html/rfc7946#section-3.1.8) for more information.
     case geometryCollection(GeometryCollection)
 
--- Sources/GeoJSON/Geometry/GeometryCollection.swift
+++ Sources/GeoJSON/Geometry/GeometryCollection.swift
@@ -1,4 +1,4 @@
 /// A heterogeneous composition of other `Geometry` objects. For example, a `Geometry` object in the shape of a lowercase roman "i" can be composed of one
-/// `Point` and one `LineString`.
+/// `Point` and one `LineString`. Please try and avoid nested `GeometryCollection`s to maximize interopability.
 /// See [RFC7946 Section 3.1.8](https://tools.ietf.org/html/rfc7946#section-3.1.8) for more information.
 public typealias GeometryCollection = [Geometry]
