5eaca199f073670c1a9fd8d0abbcf45cfdcd28d7
Kilian Koeltzsch <me@kilian.io>
2020-07-11 02:24:41 +0200

    Add a few possibly helpful BoundingBox factory methods

--- Sources/GeoJSON/BoundingBox.swift
+++ Sources/GeoJSON/BoundingBox.swift
@@ -53,3 +53,32 @@ public struct BoundingBox: Equatable, Codable {
         }
     }
 }
+
+// See [RFC7946 Section 5.3](https://tools.ietf.org/html/rfc7946#section-4).
+extension BoundingBox {
+    /// A bounding box that contains the North Pole. Viewed on a globe, this bounding box approximates a spherical cap bounded by the minimum latitude circle.
+    public static func containsNorthPole(minimumLatitude: Double) -> BoundingBox {
+        BoundingBox(southWesterly: .init(longitude: -180.0, latitude: minimumLatitude),
+                    northEasterly: .init(longitude: 180.0, latitude: 90.0))
+    }
+
+    /// A bounding box that contains the South Pole.
+    public static func containsSouthPole(maximumLatitude: Double) -> BoundingBox {
+        BoundingBox(southWesterly: .init(longitude: -180.0, latitude: -90.0),
+                    northEasterly: .init(longitude: 180.0, latitude: maximumLatitude))
+    }
+
+    /// A bounding box that just touches the North Pole and forms a slice of an approximate spherical cap when viewed on a globe.
+    public static func touchesNorthPole(westLongitude: Double, minimumLatitude: Double, eastLongitude: Double)
+    -> BoundingBox {
+        BoundingBox(southWesterly: .init(longitude: westLongitude, latitude: minimumLatitude),
+                    northEasterly: .init(longitude: eastLongitude, latitude: 90.0))
+    }
+
+    /// A bounding box that just touches the South Pole and forms a slice of an approximate spherical cap when viewed on a globe.
+    public static func touchesSouthPole(westLongitude: Double, eastLongitude: Double, maximumLatitude: Double)
+    -> BoundingBox {
+        BoundingBox(southWesterly: .init(longitude: westLongitude, latitude: -90.0),
+                    northEasterly: .init(longitude: eastLongitude, latitude: maximumLatitude))
+    }
+}
